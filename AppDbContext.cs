c2cd56b37ff7493951d1cade494b084b28df3628
NightFuryL <levodesskiy@gmail.com>
2026-02-28 11:05:53 +0200

    Add project files.

---
+++ AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using practiceEFDapper.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using practiceEFDapper.Entities;
+
+
+namespace practiceEFDapper
+{
+    class AppDbContext : DbContext
+    {
+        private readonly string _connectionString = @"Data Source=NIGHTFURY\LEVMSSQLSERVER;Initial Catalog=AcademyDB;Integrated Security=True; TrustServerCertificate=True";
+        public DbSet<Student> Students { get; set; }
+
+        public AppDbContext()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appconfig.json").Build();
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using practiceEFDapper.Entities;
+using System;
+using System.Text;
+
+namespace practiceEFDapper;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        AppDbContext db = new AppDbContext();
+
+    }
+}
